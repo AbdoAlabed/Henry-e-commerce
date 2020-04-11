9808846bc1c4fb2120c3438f89b09ed73fc5f4de
AbdoAlabed <henry_3030@icloud.com>
2020-04-11 05:48:47 +0200

    add collection item and appling styles for C.preview , C.item

--- henry-clothing/src/components/collection_preview/collection.preview.jsx
+++ henry-clothing/src/components/collection_preview/collection.preview.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 
+import "./collection.preview.scss";
+import CollectionItem from "../collection_item/collection.item";
 const CollectionPreview = ({ title, items, key }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      {items
-        .filter((item, index) => index < 4)
-        .map((item) => (
-          <div>{item.name}</div>
-        ))}
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map(({ name, imageUrl, price, id }) => (
+            <CollectionItem
+              key={id}
+              name={name}
+              price={price}
+              imageUrl={imageUrl}
+            />
+          ))}
+      </div>
     </div>
   );
 };
